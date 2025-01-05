bdcd0642d633e46d38606aff9c3c0356c47436cc
tpmail <postmail0287@mail.ru>
2025-01-05 22:54:21 +0300

    feat: add 5 tasks

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  return /^([A-F\d]{2}-){5}[A-F\d]{2}$/i.test(n);
 }
 module.exports = {
   isMAC48Address
